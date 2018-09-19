d26a805bb2d6e4129de86fcf00a2a2d65213177b
Codeater7 <pokharelsujan134@gmail.com>
2018-09-20 00:07:37 +0300

    Add files via upload

---
+++ Exercise1a3.ino
@@ -0,0 +1,19 @@
+/*
+  Blink
+Exercise1a3
+Authors: Sujan Pokharel and Prabin Pandey
+*/
+
+const int LED= 13;
+void setup() {
+  // initialize digital pin LED as an output.
+  pinMode(LED, OUTPUT);
+}
+
+
+void loop() {
+  digitalWrite(LED, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(LED, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+}
